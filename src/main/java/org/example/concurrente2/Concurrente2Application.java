691a9614cfe34521dffe59a7e5db9c19fa147dad
ffernandoss <fsantama@myuax.com>
2024-11-10 11:49:18 +0100

    Initial commit

---
+++ src/main/java/org/example/concurrente2/Concurrente2Application.java
@@ -0,0 +1,13 @@
+package org.example.concurrente2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Concurrente2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Concurrente2Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/concurrente2/Concurrente2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.concurrente2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Concurrente2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
